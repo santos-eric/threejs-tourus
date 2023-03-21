1aa51bd514f3bd2e0805b602a47ffa3c0764e0be
santos-eric <esantosdev77@gmail.com>
2023-03-20 19:51:48 -0600

    normalMap addedand GUI control added, no errors

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'dat.gui';
 
+// Loading
+const textureLoader = new THREE.TextureLoader();
+
+const normalTexture = textureLoader.load('/static/textures/NormalMap.png');
+
 // Debug
 const gui = new dat.GUI();
 
@@ -13,24 +18,41 @@ const canvas = document.querySelector('canvas.webgl');
 const scene = new THREE.Scene();
 
 // Objects
-const geometry = new THREE.TorusGeometry(0.7, 0.2, 16, 100);
+const geometry = new THREE.SphereGeometry(0.5, 64, 64);
 
 // Materials
+const material = new THREE.MeshStandardMaterial();
+material.metalness = 0.7;
+material.roughness = 0.2;
+material.normalMap = normalTexture;
 
-const material = new THREE.MeshBasicMaterial();
-material.color = new THREE.Color(0xff0000);
+material.color = new THREE.Color(0x292929);
 
 // Mesh
 const sphere = new THREE.Mesh(geometry, material);
 scene.add(sphere);
 
 // Lights
-
 const pointLight = new THREE.PointLight(0xffffff, 0.1);
 pointLight.position.x = 2;
 pointLight.position.y = 3;
 pointLight.position.z = 4;
 scene.add(pointLight);
+
+const pointLight2 = new THREE.PointLight(0xff0000, 2);
+// pointLight.position.x = 2;
+// pointLight.position.y = 3;
+// pointLight.position.z = 4;
+pointLight2.position.set(1, 1, 1);
+pointLight2.intensity = 1;
+
+// scene.add(pointLight);
+scene.add(pointLight2);
+
+gui.add(pointLight2.position, 'x').min(-3).max(3).step(0.01);
+gui.add(pointLight2.position, 'y').min(-6).max(6).step(0.01);
+gui.add(pointLight2.position, 'z').min(-3).max(3).step(0.01);
+gui.add(pointLight2, 'intensity').min(0).max(10).step(0.01);
 
 /**
  * Sizes
@@ -78,6 +100,7 @@ scene.add(camera);
  */
 const renderer = new THREE.WebGLRenderer({
 	canvas: canvas,
+	alpha: true,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
